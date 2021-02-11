cafe10a57236e7940a71fd1d7d981a250fbaff9e
NikolaiSurodin <kolya_12.93@mail.ru>
2021-02-11 14:34:19 +0700

    WTF

---
+++ src/components/DaySpan.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <DaySpanVuetify />
+  </div>
+</template>
+
+<script>
+import DaySpanVuetify from 'dayspan-vuetify'
+export default {
+  name: "DaySpan",
+  components: {
+    DaySpanVuetify
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import DaySpanVuetify from 'dayspan-vuetify'
+import Vuetify from "vuetify";
+
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
+
+
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
+Vue.use(DaySpanVuetify, {
+  methods: {
+    getDefaultEventColor: () => '#1976d2'
+  }
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App)
+});
