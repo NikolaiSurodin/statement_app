eefcb0c142136a45f9b03a4679c363ae0eeb880e
Anton B <anton@gdsln.com>
2021-02-11 16:36:47 +0700

    * Added correct CSS ways * Installed material icons * Loadeed Vuetify from Dayspan local plugins * imported VIcon from original Vuetify *

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ export default {
 </script>
 
 <style lang="css">
+
 body, html, #app, #dayspan {
   font-family: Roboto, sans-serif;
   width: 100%;
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import vuetify from 'vuetify'
+import vuetify from 'dayspan-vuetify-2/src/plugins/vuetify'
 import DaySpan from 'dayspan-vuetify-2'
 
-const vuetifyOptions = {}
+import VIcon from 'vuetify'
+
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'dayspan-vuetify-2/src/styles/app.scss'
 
 Vue.config.productionTip = false
 Vue.use(vuetify)
+Vue.use(VIcon)
 Vue.use(DaySpan, {
   methods: {
     getDefaultEventColor: () => '#1976d2'
@@ -15,7 +20,7 @@ Vue.use(DaySpan, {
 });
 
 new Vue({
-  vuetify: new vuetify(vuetifyOptions),
+  vuetify,
   el: '#app',
   render: h => h(App)
 });
