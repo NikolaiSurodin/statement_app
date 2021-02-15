8aa582b055ca70e72b2e17277f69b09d016b5547
Anton B <anton@gdsln.com>
2021-02-15 17:37:57 +0700

    * check it out

--- src/main.js
+++ src/main.js
@@ -1,23 +1,44 @@
 import Vue from 'vue'
-import App from './App.vue'
 
-import vuetify from 'dayspan-vuetify-2/src/plugins/vuetify'
-import DaySpan from 'dayspan-vuetify-2'
+import vuetify from 'vuetify'
+import DaySpan from 'dayspan-vuetify'
 
 import VIcon from 'vuetify'
 
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import 'dayspan-vuetify-2/src/styles/app.scss'
+import 'dayspan-vuetify/src/styles/app.scss'
+import { dsBind, dsDefaults, dsMergeOptions, dsMergeValidate } from 'dayspan-vuetify/src/functions'
+import { default as Component } from 'dayspan-vuetify/src/component'
 
 Vue.config.productionTip = false
 Vue.use(vuetify)
 Vue.use(VIcon)
+
+// $dayspan is just another reactive component
+var $dayspan = new Vue(dsMergeOptions({ }, Component))
+// allow directives to access $dayspan
+Vue.$dayspan = $dayspan
+// allow components to access $dayspan
+Vue.prototype.$dayspan = $dayspan
+// allow components to pull & merge default component props into given
+// component props.
+Vue.prototype.$dsValidate = dsMergeValidate
+Vue.prototype.$dsDefaults = dsDefaults
+// allow v-bind="{$scopedSlots}"
+Vue.prototype._b = dsBind(Vue.prototype._b)
+// Call initialization functions
+$dayspan.init()
+
+console.log(Vue.prototype)
+
 Vue.use(DaySpan, {
   methods: {
     getDefaultEventColor: () => '#1976d2'
   }
 });
+
+import App from './App.vue'
 
 new Vue({
   vuetify,
